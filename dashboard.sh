80e894081ea46ff202ad2f913f143923fc599bd9
Dániel Kántor <git@daniel-kantor.com>
2019-09-02 14:39:35 +0200

    use newer just-dashboard-desktop

--- dashboard.sh
+++ dashboard.sh
@@ -4,4 +4,4 @@ rm -rf reports/
 tar -xJf <(curl https://raw.githubusercontent.com/kantord/pricedb/master/price.2010.db.tar.xz)
 mv price.2010.db price.db
 make ledgerfile=$1 currency=$2 -j -B
-./node_modules/just-dashboard-desktop/dist/linux-unpacked/just-dashboard-desktop ./.dashboard.yml
+just-dashboard ./.dashboard.yml
